eb048a186f2569bf812528e08f2ddc8393e308b7
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-02-20 20:14:50 -0300

    criado widget de drawer

--- app/lib/src/pages/home/home_page.dart
+++ app/lib/src/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../shared/widgets/user_image_button.dart';
+import 'widgets/custom_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -27,45 +28,7 @@ class _HomePageState extends State<HomePage> {
         icon: const Icon(Icons.add_outlined),
         label: const Text('nova lista'),
       ),
-      drawer: NavigationDrawer(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 12,
-              top: 28,
-              right: 12,
-              bottom: 12,
-            ),
-            child: Text(
-              'opções',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-          ),
-          NavigationDrawerDestination(
-            icon: const Icon(Icons.sync_outlined),
-            selectedIcon: const Icon(Icons.sync),
-            label: Row(
-              children: [
-                const Text(
-                  'sincronizar',
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Text(
-                  '12/12/12 12:12',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-          ),
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.settings_outlined),
-            selectedIcon: Icon(Icons.settings),
-            label: Text('configurações'),
-          ),
-        ],
-      ),
+      drawer: const MyCustomDrawer(),
       body: Column(
         children: [
           Padding(
---
+++ app/lib/src/pages/home/widgets/custom_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyCustomDrawer extends StatelessWidget {
+  const MyCustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return NavigationDrawer(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 12,
+            top: 28,
+            right: 12,
+            bottom: 12,
+          ),
+          child: Text(
+            'opções',
+            style: theme.textTheme.titleSmall,
+          ),
+        ),
+        NavigationDrawerDestination(
+          icon: const Icon(Icons.sync_outlined),
+          selectedIcon: const Icon(Icons.sync),
+          label: Row(
+            children: [
+              const Text(
+                'sincronizar',
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                '12/12/12 12:12',
+                style: theme.textTheme.bodySmall,
+              ),
+            ],
+          ),
+        ),
+        const NavigationDrawerDestination(
+          icon: Icon(Icons.settings_outlined),
+          selectedIcon: Icon(Icons.settings),
+          label: Text('configurações'),
+        ),
+      ],
+    );
+  }
+}
