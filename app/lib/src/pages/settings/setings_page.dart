560e018cc6db8db01d97492324e57b489fa848c7
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2023-02-20 21:12:58 -0300

    adicionado widgets à tela de config

--- app/lib/src/pages/settings/setings_page.dart
+++ app/lib/src/pages/settings/setings_page.dart
@@ -10,11 +10,55 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('configurações'),
+        title: const Text('listinha'),
       ),
-      body: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text('configurações', style: textTheme.titleLarge),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+            child: Text('tema', style: textTheme.titleMedium),
+          ),
+          RadioListTile<ThemeMode>(
+            title: const Text('sistema'),
+            value: ThemeMode.system,
+            groupValue: ThemeMode.light,
+            onChanged: (mode) {},
+          ),
+          RadioListTile<ThemeMode>(
+            title: const Text('claro'),
+            value: ThemeMode.light,
+            groupValue: ThemeMode.light,
+            onChanged: (mode) {},
+          ),
+          RadioListTile<ThemeMode>(
+            title: const Text('escuro'),
+            value: ThemeMode.dark,
+            groupValue: ThemeMode.light,
+            onChanged: (mode) {},
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+            child: Text('controle de dados', style: textTheme.titleMedium),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: OutlinedButton(
+              onPressed: () {},
+              child: const Text('apagar cache e reiniciar o app'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
